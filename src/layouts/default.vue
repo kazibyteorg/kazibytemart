357c7d63497371ff804b7ac4a7924517a7ed955e
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-03-17 10:46:23 +0600

    /nuxt-config

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+   
+      <main class="">
+        <slot />
+      </main>
+    </div>
+  </template>
+  
+  <script setup>
+//   import Header from "@/components/common/Header.vue";
+//   import 'primeicons/primeicons.css'
+  
+
+  </script>
+  
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum eaque unde ex nisi similique natus amet obcaecati culpa doloribus repudiandae adipisci vitae quibusdam et impedit, inventore nihil odit a enim?
+</template>
